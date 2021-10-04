a1c61d35883b9e6cf592374319df7a67c1c32595
ppal31 <pal.prashant31@gmail.com>
2021-10-04 22:46:40 +0530

    Added custom balancer

---
+++ test/integration/balancer_test.go
@@ -0,0 +1,27 @@
+package integration
+
+import (
+	"github.com/ppal31/gochat/cli/client"
+	"testing"
+)
+
+func TestChatService_Ping(t *testing.T) {
+	c := client.NewClient([]string{"127.0.0.1:2181"})
+	err := c.Ping()
+	if err != nil {
+		t.Errorf("Failed to ping %v", err.Error())
+	}
+}
+
+func TestChatService_Chat(t *testing.T) {
+	c := client.NewClient([]string{"127.0.0.1:2181"})
+	err := c.Chat("5001")
+	if err != nil {
+		t.Errorf("Failed to ping %v", err.Error())
+	}
+
+	err = c.Chat("5002")
+	if err != nil {
+		t.Errorf("Failed to ping %v", err.Error())
+	}
+}
